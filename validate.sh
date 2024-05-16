f03ce386535c33803b5f9c2668d2e6537274d09f
LeoY <minherz@users.noreply.github.com>
2024-05-16 00:27:31 +0000

    Fix validate.sh
    Add missing `fi`.
    Enclose variables into quotes to protect against input with spaces.

--- validate.sh
+++ validate.sh
@@ -4,7 +4,7 @@ set -eu
 
 _error_report() {
   echo >&2 "Exited [$?] at line $(caller):"
-  cat -n $0 | tail -n+$(($1 - 3)) | head -n7 | sed "4s/^\s*/>>> /"
+  cat -n "$0" | tail -n+$(($1 - 3)) | head -n7 | sed "4s/^\s*/>>> /"
 }
 trap '_error_report $LINENO' ERR
 
@@ -13,15 +13,15 @@ read -rp "Please input digital badge id: " badge_id
 if [[ -z "${badge_id}" ]]; then
     echo "Cannot validate certification without digital badge id"
     exit 1
-
+fi
 read -rp "Please input an email address used for certification exam registration: " subscriber_email
 if [[ -z "${subscriber_email}" ]]; then
     echo "Cannot validate certification without email address"
     exit 1
-
+fi
 invalid=0
-data=$(curl -sH "Content-Type: application/json" https://api.credly.com/v1/obi/v2/badge_assertions/$badge_id)
-exp_date=$(echo -n $data | jq -r .expires)
+data=$(curl -sH "Content-Type: application/json" "https://api.credly.com/v1/obi/v2/badge_assertions/$badge_id")
+exp_date=$(echo -n "$data" | jq -r .expires)
 now=$(TZ=UTC date +"%Y-%m-%d")"T"$(TZ=UTC date +"%T")".000Z"
 
 #Validate certificate exists
@@ -37,8 +37,8 @@ if [[ $exp_date < $now ]]; then
 fi
 
 # Validate that certificate is issued to the provided email
-digest=($(echo -n ${subscriber_email,,} | sha256sum))
-valid_digest=$(echo -n $data | jq -r .recipient.identity)
+digest=($(echo -n "${subscriber_email,,}" | sha256sum))
+valid_digest=$(echo -n "$data" | jq -r .recipient.identity)
 if [[ $valid_digest != "sha256\$$digest" ]]; then
     echo "Certificate was not issued to ${subscriber_email}"
     invalid=1
@@ -46,5 +46,6 @@ fi
 
 if [ $invalid -eq 0 ]; then
    echo "Certificate is valid"
-   echo $(echo -n $data | jq .evidence)
+   result=$(echo -n "$data" | jq .evidence)
+   echo "$result"
 fi
